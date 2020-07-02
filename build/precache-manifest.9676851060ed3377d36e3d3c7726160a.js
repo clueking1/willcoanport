3f0d4e756d4f6eb6b80bbfc7045b69e4905601f5
clueking1 <wcoan300@gmail.com>
2020-07-02 18:30:34 -0400

    changed http to https

---
+++ build/precache-manifest.9676851060ed3377d36e3d3c7726160a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b8c3a4628bfe99fa4c1fa0d72471bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38c5e3e55dd781c2e941",
+    "url": "/static/css/main.63426982.chunk.css"
+  },
+  {
+    "revision": "28251bbdbfdbc9142c39",
+    "url": "/static/js/2.4abb9bf5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4abb9bf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c5e3e55dd781c2e941",
+    "url": "/static/js/main.6e31fe24.chunk.js"
+  },
+  {
+    "revision": "614de22ac45fb60ef670",
+    "url": "/static/js/runtime-main.0cf80715.js"
+  },
+  {
+    "revision": "af55e414dbc2572aa375116ac0cd7426",
+    "url": "/static/media/WillCoanRe.af55e414.pdf"
+  },
+  {
+    "revision": "d9bf26cca2cf3937f2289707936362ce",
+    "url": "/static/media/dayplanner.d9bf26cc.jpg"
+  },
+  {
+    "revision": "0736fb6bc7a6b4f79b269119e0000cec",
+    "url": "/static/media/pp.0736fb6b.jpg"
+  },
+  {
+    "revision": "139da310484bd506f69398a1d2174029",
+    "url": "/static/media/recom.139da310.jpg"
+  },
+  {
+    "revision": "79412b831183ce60f56876c3458c97da",
+    "url": "/static/media/syp.79412b83.jpg"
+  },
+  {
+    "revision": "e3e315a34a1a913a8bba55718a94177b",
+    "url": "/static/media/weather.e3e315a3.jpg"
+  },
+  {
+    "revision": "6470ee1a7b317965304e6f418c1f8a98",
+    "url": "/static/media/zodiac.6470ee1a.jpg"
+  }
+]);
